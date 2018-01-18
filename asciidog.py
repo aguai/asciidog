46856f641454dadd1b40d09517805fe1a608dd7d
aguai <aguaithefreak@gmail.com>
2018-01-19 02:26:04 +0800

    try a recursive version

--- asciidog.py
+++ asciidog.py
@@ -2,42 +2,32 @@
 from re import match, sub
 f = sub(r"\/{4}[^\/{4}]+\/{4}|\/\/\ .+\n", '',
         open('resource/test2.adoc').read()).splitlines()
-Begin = r'^\={3}\ '
-
-t = []
 
 
 def Blockenize(S):
-    x = []
-    while match(Begin, S[0]) == None:
-
-        if len(S) == 0:
-            print('no more')
-            return
-        elif len(S) == 1:
-            x.append(S[0])
-            print(S[0])
-            print('______END________')
-            t.append(x)
+    sexRegx = r"(?s)\={3}\s"
+    done = []
+    temp = []
+    while isinstance(S[0], list):
+        done.append(S.pop(0))
+    if match(sexRegx, S[0]):
+        temp.append(S.pop(0))
+    while match(sexRegx, S[0]) is None:
+        if len(S) == 1:
+            temp.append(S[0])
             S.pop(0)
-            return
+            break
         else:
-            x.append(S[0])
-            print(S[0])
+            temp.append(S[0])
             S.pop(0)
+    done.append(temp)
 
-    t.append(x)
-    print('add a section')
-
-    if match(Begin, S[0]):
-        x.append(S[0])
-        S.pop(0)
+    S = done + S
+    if not isinstance(S[-1], list):
         Blockenize(S)
+    else:
+        pass
+    return S
 
 
-Blockenize(f)
-
-for i in t:
-    print("section ***********\n")
-    print(i)
-    print("*******************\n")
+print(Blockenize(f))
